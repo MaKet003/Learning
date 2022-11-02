0d16b259857694aac0ddd45ab50ab971a638753f
MaKet003 <ManuelKetz92@gmail.com>
2022-11-02 15:12:19 +0100

    files added

--- Xamarin/FirstMauiApp/FirstMauiApp/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/FirstMauiApp.AssemblyInfo.cs
+++ Xamarin/FirstMauiApp/FirstMauiApp/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/FirstMauiApp.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "CB81C0FE-2ED5-40E1-87A2-DE5E2375D915")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "FirstMauiApp")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
