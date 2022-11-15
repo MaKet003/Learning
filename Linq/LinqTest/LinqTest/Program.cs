5ec6918f1b15e836be9946f7a6148e35de49bcbc
MaKet003 <ManuelKetz92@gmail.com>
2022-11-15 19:31:55 +0100

    little Linq Test

--- Linq/LinqTest/LinqTest/Program.cs
+++ Linq/LinqTest/LinqTest/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace LinqTest
+{
+    public class LinqTest
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = { "Peter", "Hartmut", "Jesika", "Ines" };
+
+            var LinqName = from name in names
+                           where name.Contains('t')
+                           select name;
+
+            foreach(var name in LinqName)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
